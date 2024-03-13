8d33f4d79b0cc8cb6027e276fc15aef21d1e6c2d
Moviessgallery <88574086+venky2384yashubujjishobha@users.noreply.github.com>
2024-03-13 17:01:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Moviessgallery_Bot.git /Moviessgallery_Bot 
+  git clone https://github.com/VJBots/MoviessFiles_Bot.git /MoviessFiles_Bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Moviessgallery_Bot 
+  git clone $UPSTREAM_REPO /MoviessFiles_Bot 
 fi
-cd /Moviessgallery_Bot 
+cd /MoviessFiles_Bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
